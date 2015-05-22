a6916a9a87d1f24674fb134e1b56bd73a7fbae0a
Aaron Echols <aechols2@hawk.iit.edu>
2015-05-21 21:15:46 -0500

    queue is working

--- kitchen-server/runecast.py
+++ kitchen-server/runecast.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, redirect, url_for
 import uuid
 
 app = Flask(__name__)
@@ -9,10 +9,17 @@ queue = []
 def list_current():
 	return 'Welcome to the Rune Cast!'
 
+@app.route('/next')
+def next():
+	if len(queue) < 2:
+		return 'No tracks queued!'
+	else:
+		return str(queue[1])
+
 @app.route('/append', methods=['POST'])
 def append():
 	track = {}
-	trackID = uuid4()
+	trackID = uuid.uuid4()
 	
 	track['title'] = request.form['title']
 	track['location'] = request.form['location']
@@ -20,6 +27,7 @@ def append():
 	
 	track['protocol'] = derive_protocol(request.form['location'])
 	queue.append(track)
+	return redirect(url_for('next'))
 
 def derive_protocol(location):
 	return 'local' # TODO
