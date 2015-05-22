9a9397f01f34956fcdf03d09bd3318e07d71e44d
Aaron Echols <aechols2@hawk.iit.edu>
2015-05-21 21:06:42 -0500

    fixed method type

--- kitchen-server/runecast.py
+++ kitchen-server/runecast.py
@@ -9,14 +9,17 @@ queue = []
 def list_current():
 	return 'Welcome to the Rune Cast!'
 
-@app.route('/append', method='POST')
+@app.route('/append', methods=['POST'])
 def append():
 	track = {}
 	trackID = uuid4()
 	
 	track['title'] = request.form['title']
 	track['location'] = request.form['location']
+	track['artist'] = request.form['artist']
+	
 	track['protocol'] = derive_protocol(request.form['location'])
+	queue.append(track)
 
 def derive_protocol(location):
 	return 'local' # TODO
